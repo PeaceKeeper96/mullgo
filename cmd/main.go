85edd886e06f7e06c37fe382115b28bf644a5f87
PeaceKeeper96 <peacekeepr96@gmail.com>
2026-03-10 15:22:39 +0000

    Update to useable state #2

--- cmd/main.go
+++ cmd/main.go
@@ -5,66 +5,39 @@ import (
 	mullvad "mullgo"
 )
 
-type Client struct {
-	// AccountNumber is the mullvad account number to use.
-	AccountNumber string
-	// Countries is a list of countries the configuration can be made from.
-	Countries []string
-}
-
-type Response struct {
-	data string
-}
-
 func main() {
 	sampleClient := mullvad.NewClient(
 		"",
 		[]string{"ch-zrh", "de-fra"},
 	)
-	// sampleRelay := &mullvad.Relay{
-	// 	Name: "testrelay",
-	// 	IPv4Addr: "123.123.123.123",
-	// 	MultihopPort: 45,
-	// 	PublicKey: "testkey",
-	// 	CityCode: "SAMP",
-	// 	Hostname: "hostnamesample",
-	// }
 
-	// config1, err := mullvad.GenerateConfig(
-	// 	sampleClient,
-	// 	[]mullvad.Relay{
-	// 		*sampleRelay,
-	// 	},
-	// )
-	// if err != nil {
-	// 	fmt.Println("ERROR")
-	// }
-	// fmt.Println(config1)
-
-	// jsonResp := Response{}
-	// mullvad.GetJson("https://api.mullvad.net/wg", &jsonResp)
-
-	token := mullvad.GetToken(sampleClient.AccountNumber)
+	token, _ := mullvad.GetToken(sampleClient.AccountNumber)
 	fmt.Println("Token: ", token.AccessToken)
 	fmt.Println("Expiry: ", token.Expiry)
 
-	relays := mullvad.GetRelays(token)
+	relays, _ := mullvad.GetRelays(token)
 	filteredRelays := mullvad.RelayFilter(*sampleClient, relays)
 	fmt.Println("so back")
 	fmt.Println(filteredRelays)
 
-	pubKey, privKey := mullvad.Keygen()
+	pubKey, privKey, _ := mullvad.Keygen()
 	fmt.Println("PubKey: ", pubKey)
 	fmt.Println("PrivKey: ", privKey)
 
-	num, err := mullvad.AddKey(token, pubKey)
-	fmt.Println(num, err)
+	keyCode, err := mullvad.AddKey(token, pubKey)
+	fmt.Println(keyCode, err)
 
-	devices := mullvad.GetDevices(token)
+	devices, _ := mullvad.GetDevices(token)
 	fmt.Println("Devices: ", devices)
 
-	device := mullvad.GetDevicebyPubkey(devices, "CN7D6kEsXOlVSGFY5qAqym8ITCjA4NzGGERej8kxE3Q=")
+	device, _ := mullvad.GetDevicebyPubkey(devices, devices[0].Pubkey)
 	fmt.Println("\n", device)
+
+	removeCode, _ := mullvad.RemoveDevice(token, device.ID)
+	fmt.Println("\nIf removed, you will see 200: ", removeCode)
+
+	accountInfo, _ := mullvad.GetAccountInfo(token)
+	fmt.Println("\nAccount Info: ", accountInfo)
 }
 
 // will be our main client struct and constructor
